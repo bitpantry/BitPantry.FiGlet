131254c982014da27e13f5ba2edd811e6eadb170
Jeff Hewitt <jhewitt@credera.com>
2020-09-18 12:52:17 -0500

    Add project files.

---
+++ BitPantry.FIGlet/Figlet.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BitPantry.FIGlet
+{
+    public class Figlet
+    {
+        private Dictionary<char, string[]> _alphabet;
+        private Font _font;
+
+        public Figlet()
+        {
+            _font = Font.Standard;
+        }
+
+        public Figlet(Font font)
+        {
+            _font = font;
+        }
+
+        public Figlet(string fontFilePath)
+        {
+            _font = new Font(fontFilePath);
+        }
+
+        public string ToAsciiArt(string strText)
+        {
+            var res = "";
+            for (int i = 1; i <= _font.Height; i++)
+            {
+                foreach (var car in strText)
+                {
+                    res += this.GetCharacter(car, i);
+                }
+                res += Environment.NewLine;
+            }
+            return res;
+        }
+
+        private string GetCharacter(char car, int line)
+        {
+            var start = _font.CommentLines + ((Convert.ToInt32(car) - 32) * _font.Height);
+            var temp = _font.Lines[start + line];
+            var lineending = temp[temp.Length - 1];
+            var rx = new Regex(@"\" + lineending + "{1,2}$");
+            temp = rx.Replace(temp, "");
+            return temp.Replace(_font.HardBlank, " ");
+        }
+    }
+}
---
+++ BitPantry.FIGlet/Font.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace BitPantry.FIGlet
+{
+    public class Font
+    {
+        public string Signature { get; private set; }
+        public string HardBlank { get; private set; }
+        public int Height { get; private set; }
+        public int BaseLine { get; private set; }
+        public int MaxLenght { get; private set; }
+        public int OldLayout { get; private set; }
+        public int CommentLines { get; private set; }
+        public int PrintDirection { get; private set; }
+        public int FullLayout { get; set; }
+        public int CodeTagCount { get; set; }
+        public List<string> Lines { get; set; }
+
+        internal Font(string fontFilePath)
+        {
+            using (var fso = File.Open(fontFilePath, FileMode.Open))
+                LoadFont(fso);
+        }
+
+        internal Font(Stream stream) { LoadFont(stream); }
+
+        private void LoadFont(Stream fontStream)
+        {
+            var _fontData = new List<string>();
+            using (var reader = new StreamReader(fontStream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    _fontData.Add(reader.ReadLine());
+                }
+            }
+            LoadLines(_fontData);
+        }
+
+        private void LoadLines(List<string> fontLines)
+        {
+            Lines = fontLines;
+            var configString = Lines.First();
+            var configArray = configString.Split(' ');
+            Signature = configArray.First().Remove(configArray.First().Length - 1);
+            if (Signature == "flf2a")
+            {
+                HardBlank = configArray.First().Last().ToString();
+                Height = configArray.GetIntValue(1);
+                BaseLine = configArray.GetIntValue(2);
+                MaxLenght = configArray.GetIntValue(3);
+                OldLayout = configArray.GetIntValue(4);
+                CommentLines = configArray.GetIntValue(5);
+                PrintDirection = configArray.GetIntValue(6);
+                FullLayout = configArray.GetIntValue(7);
+                CodeTagCount = configArray.GetIntValue(8);
+            }
+        }
+
+        #region FONT FACTORY FUNCTIONS
+
+        public static Font Standard => GetFont("standard");
+        public static Font Isometric3 => GetFont("isometric3");
+        public static Font Larry3d => GetFont("larry3d");
+        public static Font Rectangles => GetFont("rectangles");
+        public static Font Big => GetFont("big");
+        public static Font Small => GetFont("small");
+
+        private static Font GetFont(string resourceName)
+        {
+            using (var stream = Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream($"BitPantry.FIGlet.Fonts.{resourceName}.flf"))
+                return new Font(stream);
+        }
+
+        #endregion
+    }
+}
